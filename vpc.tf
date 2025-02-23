44d39adf6c4a54a67a23569a767cf0d402d89c93
karthikraj98 <karthikbayanna98@gmail.com>
2025-02-23 16:34:49 +0530

    commit changes

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source = "../terraform-aws-vpc"
-  # source = "git::https://github.com/karthikraj98/terraform-aws-vpc.git?ref=main"
+  # source = "../terraform-aws-vpc"
+  source = "git::https://github.com/karthikraj98/terraform-aws-vpc.git?ref=main"
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment = var.environment
