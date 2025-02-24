9292e79a2391bafdfcc6dec790a4daa7928e9130
karthikraj98 <karthikbayanna98@gmail.com>
2025-02-24 11:08:05 +0530

    commit changes

--- variable.tf
+++ variable.tf
@@ -10,10 +10,6 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
-}
-
 variable "common_tags" {
     default = {
       Project = "expense"
